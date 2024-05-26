336c842bd1f6dd2179c91e852b404cf77849f837
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-05-26 12:29:03 -0400

    removed candy and student models and related content. kept Notes models as this is similar to what I need for goal chart

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ const app = express();
 const cors = require("cors");//allows to receive requests on cross-origins
 const ConnectToDb = require("./config/connectToDb");//This pulls our Mongoose(MongoDB) connection into the application
 const notesRouter = require("./routes/notes");
-const candyRouter = require("./routes/candy");
-const studentRouter = require("./routes/student");
+
 //Separation of concerns: Models > Controller > Router > Main Server JS file
 // const Note = require("./models/note");  //moved the model and imported to the corresponding controller file
 // const Candy = require("./models/candy");  //moved the model and imported to the corresponding controller file
@@ -17,8 +16,7 @@ app.use(cors());//use as middleware
 app.use(express.json({extended: true}));//express doesn't convert to JSON naturally
 app.use(express.urlencoded({extended: true}))
 app.use("/notes", notesRouter);
-app.use("/candies", candyRouter);
-app.use("/students", studentRouter);
+
 
 ConnectToDb(); //This initializes our connectToDb function from the config folder
 
