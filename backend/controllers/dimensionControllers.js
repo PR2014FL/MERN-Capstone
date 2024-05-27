2cb7ffa718612793ff2a37dfd4c95adfa38d63d7
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-05-27 14:12:52 -0400

    Added dimensions controller

---
+++ backend/controllers/dimensionControllers.js
@@ -0,0 +1,16 @@
+//1. Bring in the model
+const Dimension = require("../models/dimension");
+//2. I only need to fetch all to build state in the frontend
+
+const fetchAllDimensions = async (req, res) => {
+  try {
+    //1.GET all dimensions
+    const dimension = await Dimension.find({});
+    //2. Send all as response
+    res.json({ dimensions: dimension });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = fetchAllDimensions;
--- backend/models/dimension.js
+++ backend/models/dimension.js
@@ -8,3 +8,4 @@ const dimensionSchema = new mongoose.Schema({
 const Dimension = mongoose.model("Dimension", dimensionSchema);
 
 module.exports = Dimension;
+//Separation of concerns: model > controller > route > import to server.js
